a02b68ee55457484116781192e875c5124797ede
cosmo1809 <omsoc1809@gmail.com>
2024-03-05 10:00:02 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,11 +15,30 @@ namespace CMP1903_A1_2324
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
 
+        private Random roll;
+        private int newValue;
         //Property
 
+        public Die()
+        {
+            roll = new Random();
+            diceRoller();
+        }
+
+        public int CurrentValue
+        {
+            get { return newValue; }
+            private set { newValue = value; }
+        }
 
         //Method
+        public int diceRoller()
+        {
+            newValue = roll.Next(1, 7);
+            return newValue;
+        }
 
 
     }
 }
+
